82f21ebfd8b248973a534aef212646d10bfaf381
vibrantsudarshan <sudarshan.c@v2solutions.com>
2023-09-07 21:41:17 +0530

    1600- 3200

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         </div>
         <div className="gpt3__header-content__people">
           <img src={people} alt="people"/>
-          <p>1,600 people requested access a visit in last 24 hours</p>
+          <p>3,200 people requested access a visit in last 24 hours</p>
         </div>
       </div>
         <div className="gpt3__header-image">
