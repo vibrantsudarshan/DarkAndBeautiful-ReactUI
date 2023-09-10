6e12ffaa9a24bdb6d5665982287aa93c3fb6d17a
vibrantsudarshan <105799949+vibrantsudarshan@users.noreply.github.com>
2023-09-10 22:12:58 +0530

    Update Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
-          Let’s Build Something together amazing with GPT-3 OpenAI @V2 Solutions on Sept 8th @1110IST!
+          Let’s Build Something together amazing with GPT-3 OpenAI @V2 Solutions on Sept 10th @2212IST!
         </h1>
         <p>At V2 Solutions, We are transforming Industries with Cutting-Edge AI Services and Expertise to Drive Efficiency, Growth, and Innovation!</p>
         <div className="gpt3__header-content__input">
@@ -17,7 +17,7 @@ const Header = () => {
         </div>
         <div className="gpt3__header-content__people">
           <img src={people} alt="people"/>
-          <p>3,200 people requested access a visit in last 24 hours</p>
+          <p>4,800 people requested access a visit in last 24 hours</p>
         </div>
       </div>
         <div className="gpt3__header-image">
